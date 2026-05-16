8e2f221813e7377206db93a5957cf59692cf745e
sivakumardev25 <sivakumarr2000@gmail.com>
2026-05-16 13:58:01 +0530

    Removing console

--- frontend/reset-frontend/src/api.js
+++ frontend/reset-frontend/src/api.js
@@ -3,5 +3,5 @@ import axios from "axios";
 const API = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
 });
-console.log(process.env.REACT_APP_API_URL);
+// console.log(process.env.REACT_APP_API_URL);
 export default API;
